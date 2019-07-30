eab33b3b6fa6e445df61b8baa609daab521210b2
Dylan Helpling <dkhelpling@gmail.com>
2019-07-30 05:59:16 -0500

    foundation for new project

---
+++ src/components/Footer.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <footer class="page-footer font-small stylish-color-dark fixed-bottom bg-dark">
+      <div class="container">
+        <div class="row"></div>
+      </div>
+      <div class="footer-copyright text-center py-3">
+        © 2019 Copyright |
+        <a href="https://mdbootstrap.com/education/bootstrap/">DylanHelpling.com</a>
+      </div>
+    </footer>
+  </div>
+</template>
+
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer-copyright.text-center.py-3 {
+  color: white;
+}
+</style>
